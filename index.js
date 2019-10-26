2cdb5965b1981c8358a7819c4563e14707c85950
Adam Webster <adamwebster@Adams-Mac-Pro.local>
2019-10-26 15:04:07 -0400

    added index.js to test on ipad

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+
+// Setup
+const app = express();
+const port = 3090;
+const config = require('./node_modules/@vue/cli-service/webpack.config.js');
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+  publicPath: "/",
+  serverSideRender: true,
+  watchOptions: {
+    // Due to iOS devices memory constraints
+    // disabling file watching is recommended 
+    ignored: /.*/
+  }
+});
+app.use(middleware);
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html', { root: __dirname });
+});
+
+// Launch app
+app.listen(port, () => {
+  console.log(
+    'Launching app... http://localhost:' + port + '\n'
+  );
+});
+
+// Register app and middleware. Required for better
+// performance when running from play.js
+try { pjs.register(app, middleware); } catch (error) { }
